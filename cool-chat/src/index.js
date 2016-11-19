4c1b17e4822627c4401a7f1606c4c97bea0ccc7c
ecezalp <ozale272@newschool.edu>
2016-11-19 16:38:10 -0500

    working on plugging

--- cool-chat/src/index.js
+++ cool-chat/src/index.js
@@ -1,38 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
 
 import { Provider } from 'react-redux';
-
-import { createStore, applyMiddleware, compose } from 'redux';
-import {reduxReactFirebase, firebaseStateReducer} from 'redux-react-firebase'
-
-import ReduxPromise from 'redux-promise';
-import rootReducer from './reducers';
-
-import { fetchMessages } from './actions'
-
-const store = createStore(rootReducer, applyMiddleware(ReduxPromise))
-
-const config = {
-    apiKey: "AIzaSyB0QAJAnXUD3YlpqtSRMll5HpsQkWOc1-U",
-    authDomain: "cool-chat-app-ecf70.firebaseapp.com",
-    databaseURL: "https://cool-chat-app-ecf70.firebaseio.com/",
-    storageBucket: ""
-}
-
-const createStoreWithFirebase = compose(
-    reduxReactFirebase(config),
-    applyMiddleware(ReduxPromise)
-)(store)
-
-store.dispatch(fetchMessages())
-
-
+import {store} from './reducers'
 
 ReactDOM.render(
     <Provider store={store} >
